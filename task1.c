3dae9d2b56d874efd7a18e6d7b7669224c1a900f
Miakah Komardley <m507k066@ku.edu>
2026-09-25 16:05:02 -0500

    Working on t1

--- task1.c
+++ task1.c
@@ -5,14 +5,17 @@
 void getNum() {
 
     int num;
-    printf("Enter the NFL score (Enter 1 to stop): \n")
-    scanf("%d", &num)
+    printf("Enter the NFL score (Enter 1 to stop): \n");
+    scanf("%d", &num);
 
 }
 
 
 
 int main() {
-    
+
+    do {
+        getNum();
+    } while (num =! 1)
 
 }
