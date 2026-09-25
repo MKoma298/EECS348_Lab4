aa2d087ea15857b7e057b2e05164c8c3d76bb240
Miakah Komardley <m507k066@ku.edu>
2026-09-25 15:48:07 -0500

    Starting files

---
+++ task1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+//Function to get user num and store it
+void getNum() {
+
+    int num;
+    printf("Enter the NFL score (Enter 1 to stop): \n")
+    scanf("%d", &num)
+
+}
+
+
+
+int main() {
+    
+
+}
