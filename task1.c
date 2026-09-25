89c23792ed4d481659428b39a764cf0a54647ef4
Miakah Komardley <m507k066@ku.edu>
2026-09-25 16:30:46 -0500

    better now

--- task1.c
+++ task1.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
+//All combos that could be made from user input
+void combos() {
 
-//Function to get user num and store it
-void getNum() {
+    int y, p, v, t, x, c, s;
+    
 
-    int num;
-    printf("Enter the NFL score (Enter 1 to stop): \n");
-    scanf("%d", &num);
+
+
+    printf("%y TD + %p pt, %v TD + FG, %t TD, %x %c pt FG, %s Safety");
 
 }
 
@@ -14,8 +16,10 @@ void getNum() {
 
 int main() {
 
-    do {
-        getNum();
-    } while (num =! 1)
+    int num;
+    printf("Enter the NFL score (Enter 1 to stop): \n");
+    scanf("%d", &num);
+
+    return 0;
 
 }
